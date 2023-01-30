9dbbc2a0a9758388293557d83a050ecc844b4ef4
Mostafa Milly <https://m.milly@github.com>
2023-01-30 07:39:41 +0200

    Add localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { Typography, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { theme } from "./theme/theme";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import { LocalizationProvider } from "./components";
+import "./i18n";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
-  return <div className="App"></div>;
+  const { i18n } = useTranslation();
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <LocalizationProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme(i18n.dir())}></ThemeProvider>
+        </QueryClientProvider>
+      </LocalizationProvider>
+    </I18nextProvider>
+  );
 }
 
 export default App;
---
+++ src/i18n.ts
@@ -0,0 +1,20 @@
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import en from "./assets/locales/en.json";
+import ar from "./assets/locales/ar.json";
+
+const resources = {
+  en,
+  ar,
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
